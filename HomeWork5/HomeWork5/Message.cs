0c73dbbaac2b272ee89501823aeb9c0d25e438c9
MrHeadshot <emelev2002@mail.ru>
2019-02-13 14:00:29 +0500

    Homework 5 added

---
+++ HomeWork5/HomeWork5/Message.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace HomeWork5
+{
+    class Message
+    {
+        /// <summary>
+        /// Выводит слова длинной N символов
+        /// </summary>
+        /// <param name="message">Сообщение</param>
+        /// <param name="n">Длина слов</param>
+        static public void CharCount(string message, int n)
+        {
+            string[] mes = ArrayFromString(message);
+
+            for (int i = 0; i < mes.Length; i++)
+            {
+                if (mes[i].Length == n)
+                {
+                    Console.WriteLine($"<{mes[i]}>");
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Удаляет из сообщения слова, оканчивающийся на указанную букву
+        /// </summary>
+        /// <param name="message">Сообщение</param>
+        /// <param name="last">Последняя буква</param>
+        /// <returns>message</returns>
+        static public string Remover(string message, char last)
+        {
+            string[] mess = ArrayFromString(message);
+
+            for (int i = 0; i < mess.Length; i++)
+            {
+                if (mess[i][mess[i].Length - 1] == last)
+                {
+                    mess[i] = "";
+                }
+            }
+            return string.Join(" ", mess);
+        }
+
+
+        /// <summary>
+        /// Самое длинное слово сообщения
+        /// </summary>
+        static public string Max ="";
+
+        /// <summary>
+        /// Определяем саммое длинное слово сообщения
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        static public string Maximum(string message)
+        {
+            
+            string[] mess = ArrayFromString(message);
+
+            for (int i = 0; i < mess.Length; i++)
+            {
+                if (mess[i].Length > Max.Length)
+                {
+                    Max = mess[i];
+                }
+            }
+            return Max;
+        }
+
+        /// <summary>
+        /// Самые длинные слова массива
+        /// </summary>
+        /// <param name="message">Сообщение</param>
+        /// <returns>StringBuilder с самыми длинными сообщениями</returns>
+        static public StringBuilder MaxString(string message)
+        {
+            string[] mess = ArrayFromString(message);
+            Maximum(message);
+
+            string words = "";
+            foreach (var mes in mess)
+            {
+                if (mes.Length == Max.Length)
+                {
+                    words += $"{mes}, ";
+                }
+            }
+            StringBuilder sb = new StringBuilder(words);
+            return sb;
+        }
+
+        /// <summary>
+        /// Проводит подщёт кол-ва повторов слов
+        /// </summary>
+        /// <param name="message">Сообщение</param>
+        /// <returns>Dictionary</returns>
+        static public Dictionary<string, int> Analyze(string message)
+        {
+            string[] mess = ArrayFromString(message);
+
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (var key in mess)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    dic[key]++;
+                }
+                else
+                {
+                    dic.Add(key, 1);
+                }
+                    
+            }
+            return dic;
+        }
+
+        static private string[] ArrayFromString(string message)
+        {
+            Regex regex = new Regex(@"\w+");
+
+            string[] Array = new string[regex.Matches(message).Count];
+
+            int i = 0;
+
+            foreach (var item in regex.Matches(message))
+            {
+                Array[i] = item.ToString();
+                i++;
+            }
+
+            return Array;
+        }
+
+
+    }
+}
---
+++ HomeWork5/HomeWork5/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region L5H1
+
+            /*
+
+            // 1.Создать программу, которая будет проверять корректность ввода логина.
+            //Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+            // а) без использования регулярных выражений;
+            // б) с использованием регулярных выражений.
+
+
+            Console.WriteLine("Введите логин:");
+            string userInput = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //a
+
+            Regex reg = new Regex(@"\b[a-zA-Z]{1}[a-zA-z0-9]{1,9}\b");
+            if (reg.IsMatch(userInput))
+            {
+                Console.WriteLine("Логин введён верно");
+            }
+            else
+            {
+                Console.WriteLine("Логин введён неверно");
+            }
+
+
+            //b
+
+            bool t = false;
+            for (int i = 0; i <9; i++)
+            {
+                if (char.IsDigit(userInput[0])  || userInput.Length > 10)
+                {
+                    t = true;
+                    break;
+                }
+            }
+            if (t)
+            {
+                Console.WriteLine("Логин введён неверно");
+            }
+            else
+            {
+                Console.WriteLine("Логин введён верно");
+            }
+            Console.ReadLine();
+
+            */
+
+            #endregion
+
+            #region L5H2
+
+            /*
+
+            string str = @"Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+            а) Вывести только те слова сообщения,  которые содержат не более n букв.
+            б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+            в) Найти самое длинное слово сообщения.
+            г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+            д) ***Создать метод, который производит частотный анализ текста.
+            В качестве параметра в него передается массив слов и текст, в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст. 
+            Здесь требуется использовать класс Dictionary.";
+
+            
+            int n = 5;
+            char rm = 'е';
+            Dictionary<string, int> dic = Message.Analyze(str);
+
+            Console.WriteLine($@"Слова содержащие {n} букв:");
+            Message.CharCount(str, 5);
+            Console.WriteLine();
+
+            Console.WriteLine($@"Новое сообщение без слов оканчивающихся на {rm}: 
+            «{ Message.Remover(str, rm)}»");
+            Console.WriteLine();
+
+            Console.WriteLine($@"Саммое длинное слово:
+            «{Message.Maximum(str)}»");
+            Console.WriteLine();
+
+            Console.WriteLine($@"Саммые длинные слова массива:
+            «{Message.MaxString(str).ToString()}»  ");
+            Console.WriteLine();
+
+            Console.WriteLine($@"Количество повторов слов:");
+            foreach (var item in dic)
+            {
+                Console.WriteLine($" Слово \"{item.Key}\" повторилось {item.Value} раз");
+            }
+            Console.WriteLine();
+
+
+            */
+            #endregion
+
+            #region L5H3
+
+            /*
+
+            //3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой. Регистр можно не учитывать:
+            //а) с использованием методов C#;
+            //б) *разработав собственный алгоритм.
+            //Например:
+            //badc являются перестановкой abcd.
+            string message1 = "< "+"badc"+" >";
+            string message2 = "< "+"abcd"+" >";
+            if (Compare(message1,message2))
+            {
+                Console.WriteLine($@"{message1} является перестановкой {message2}");
+            }
+            else
+            {
+                Console.WriteLine($@"{message1} НЕ является перестановкой {message2}");
+            }
+
+            */
+            #endregion
+
+            #region L5H4
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+        static bool Compare(string message1,string message2)
+        {
+            char[] mesArr1 = message1.ToCharArray();
+            char[] mesArr2 = message2.ToCharArray();
+
+            Dictionary<char, int> dic1 = CharCount(mesArr1);
+            Dictionary<char, int> dic2 = CharCount(mesArr2);
+
+            int trueCount = 0;
+
+            foreach (var key in dic1.Keys)
+            {
+                if (dic2.ContainsKey(key) && (dic1[key] == dic2[key]) && (dic1.Count == dic2.Count))
+                {
+                    trueCount++;
+                }
+            }
+
+            if (trueCount == dic1.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+        }
+
+        static Dictionary<char,int> CharCount(char[] charArr)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (var mes in charArr)
+            {
+                if (dic.ContainsKey(mes))
+                {
+                    dic[mes]++;
+                }
+                else
+                {
+                    dic.Add(mes, 1);
+                }
+            }
+            return dic;
+        }
+    }
+}
